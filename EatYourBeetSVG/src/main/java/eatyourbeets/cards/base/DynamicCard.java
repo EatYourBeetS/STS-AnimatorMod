ef43ce8273beb178fccbe01dd37592fbcb6ee31e
EatYourBeetSVG <EatYourBeetSVG@gmail.com>
2019-12-16 17:45:29 +0100

    DynamicCard can now also be used in combat

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/base/DynamicCard.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/base/DynamicCard.java
@@ -10,10 +10,19 @@ import java.util.function.Consumer;
 
 public class DynamicCard extends AnimatorCard
 {
+    protected DynamicCardBuilder builder;
+
     public Consumer<AbstractCard> onUpgrade;
     public TriConsumer<AbstractCard, AbstractPlayer, AbstractMonster> onUse;
 
-    protected DynamicCard(String id, CardStrings strings, String imagePath, int cost, CardType type, CardColor color, CardRarity rarity, CardTarget target, EYBCardBadge... badges)
+    public DynamicCard(DynamicCardBuilder builder)
+    {
+        this(builder.id, builder.cardStrings, builder.imagePath, builder.cost, builder.cardType, builder.cardColor, builder.cardRarity, builder.cardTarget, builder.cardBadges);
+
+        this.builder = builder;
+    }
+
+    private DynamicCard(String id, CardStrings strings, String imagePath, int cost, CardType type, CardColor color, CardRarity rarity, CardTarget target, EYBCardBadge... badges)
     {
         super(new EYBCardData(badges, strings), id, imagePath, cost, type, color, rarity, target);
     }
@@ -34,5 +43,46 @@ public class DynamicCard extends AnimatorCard
         {
             onUpgrade.accept(this);
         }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new DynamicCard(builder);
+    }
+
+    @Override
+    public AbstractCard makeStatEquivalentCopy()
+    {
+        DynamicCard copy = new DynamicCard(builder);
+
+        for (int i = 0; i < this.timesUpgraded; ++i)
+        {
+            copy.upgrade();
+        }
+
+        copy.onUpgrade = this.onUpgrade;
+        copy.onUse = this.onUse;
+        copy.name = this.name;
+        copy.target = this.target;
+        copy.upgraded = this.upgraded;
+        copy.timesUpgraded = this.timesUpgraded;
+        copy.baseDamage = this.baseDamage;
+        copy.baseBlock = this.baseBlock;
+        copy.baseMagicNumber = this.baseMagicNumber;
+        copy.baseSecondaryValue = this.secondaryValue;
+        copy.cost = this.cost;
+        copy.costForTurn = this.costForTurn;
+        copy.isCostModified = this.isCostModified;
+        copy.isCostModifiedForTurn = this.isCostModifiedForTurn;
+        copy.inBottleLightning = this.inBottleLightning;
+        copy.inBottleFlame = this.inBottleFlame;
+        copy.inBottleTornado = this.inBottleTornado;
+        copy.isSeen = this.isSeen;
+        copy.isLocked = this.isLocked;
+        copy.misc = this.misc;
+        copy.freeToPlayOnce = this.freeToPlayOnce;
+
+        return copy;
     }
 }

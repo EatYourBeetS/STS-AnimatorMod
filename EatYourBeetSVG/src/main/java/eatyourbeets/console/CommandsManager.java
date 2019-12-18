0559b8f618b988a7634b684d0c1b89697dd7a3fd
EatYourBeetSVG <EatYourBeetSVG@gmail.com>
2019-12-18 01:22:23 +0100

    THe command now only appears if you already have the file

--- EatYourBeetSVG/src/main/java/eatyourbeets/console/CommandsManager.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/console/CommandsManager.java
@@ -3,10 +3,16 @@ package eatyourbeets.console;
 import basemod.devcommands.ConsoleCommand;
 import eatyourbeets.console.commands.CreateCustomCard;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public abstract class CommandsManager
 {
     public static void RegisterCommands()
     {
-        ConsoleCommand.addCommand("createcustomcard", CreateCustomCard.class);
+        if (Files.exists(Paths.get("C:/temp/Animator-DynamicCards.json")))
+        {
+            ConsoleCommand.addCommand("createcustomcard", CreateCustomCard.class);
+        }
     }
 }

9d62e1282c7e187039ebb6e6f0260ea3622511a6
EatYourBeetSVG <EatYourBeetSVG@gmail.com>
2019-12-18 14:35:41 +0100

    Update Patchouli.java

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/colorless/Patchouli.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/colorless/Patchouli.java
@@ -7,12 +7,10 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.screens.CardRewardScreen;
 import eatyourbeets.cards.animator.special.OrbCore_Fire;
 import eatyourbeets.cards.animator.special.OrbCore_Frost;
-import eatyourbeets.cards.base.AnimatorCard;
-import eatyourbeets.cards.base.DynamicCardBuilder;
-import eatyourbeets.cards.base.EYBCardBadge;
-import eatyourbeets.cards.base.Synergies;
+import eatyourbeets.cards.base.*;
 import eatyourbeets.interfaces.markers.Spellcaster;
 import eatyourbeets.ui.EffectHistory;
 import eatyourbeets.utilities.GameActions;
@@ -21,14 +19,14 @@ import eatyourbeets.utilities.GameUtilities;
 public class Patchouli extends AnimatorCard implements Spellcaster
 {
     public static final String ID = Register(Patchouli.class.getSimpleName(), EYBCardBadge.Special);
-    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 
     public Patchouli()
     {
         super(ID, 2, AbstractCard.CardType.SKILL, AbstractCard.CardColor.COLORLESS, AbstractCard.CardRarity.RARE, AbstractCard.CardTarget.SELF);
 
-        Initialize(0,0, 1,1);
+        Initialize(0, 0, 1, 1);
 
         SetEvokeOrbCount(magicNumber);
         SetSynergy(Synergies.TouhouProject);
@@ -49,47 +47,20 @@ public class Patchouli extends AnimatorCard implements Spellcaster
         {
             GameActions.Bottom.ChannelRandomOrb(true);
         }
+
         GameActions.Bottom.GainIntellect(secondaryValue);
 
-        if(EffectHistory.TryActivateLimited(this.cardID)){
-            DynamicCardBuilder cardBuilder1 = new DynamicCardBuilder(cardID + "_Sun")
-                    .SetProperties(-2, CardType.SKILL, color, rarity, target)
-                    .SetNumbers(0, 0, 0, 0)
-                    .SetText(cardStrings.EXTENDED_DESCRIPTION[0], cardStrings.EXTENDED_DESCRIPTION[1], cardStrings.EXTENDED_DESCRIPTION[1])
-                    .SetOnUse((card, player, enemy) ->
-                    {
-                        GameActions.Bottom.MakeCardInDiscardPile(new OrbCore_Fire());
-                        for (AbstractMonster monsters : GameUtilities.GetCurrentEnemies(true))
-                        {
-                            GameActions.Bottom.ApplyBurning(p, monsters, 5);
-                        }
-                    });
-
-            DynamicCardBuilder cardBuilder2 = new DynamicCardBuilder(cardID + "_Moon")
-                    .SetProperties(-2, CardType.SKILL, color, rarity, target)
-                    .SetNumbers(0, 0, 0, 0)
-                    .SetText(cardStrings.EXTENDED_DESCRIPTION[2], cardStrings.EXTENDED_DESCRIPTION[3], cardStrings.EXTENDED_DESCRIPTION[3])
-                    .SetOnUse((card, player, enemy) ->
-                    {
-                        GameActions.Bottom.MakeCardInDiscardPile(new OrbCore_Frost());
-                        GameActions.Bottom.GainMetallicize(2);
-                    });
-
+        if (EffectHistory.TryActivateLimited(this.cardID))
+        {
             CardGroup choices = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
-            choices.group.add(cardBuilder1.Build());
-            choices.group.add(cardBuilder2.Build());
+            choices.group.add(CreateSun().Build());
+            choices.group.add(CreateMoon().Build());
 
             GameActions.Bottom.SelectFromPile(name, 1, choices)
-                    .SetOptions(false, false)
-                    .AddCallback(m, (enemy, cards) ->
-                    {
-                        if (cards.size() > 0)
-                        {
-                            cards.get(0).use(AbstractDungeon.player, (AbstractMonster) enemy);
-                        }
-                    });
+            .SetMessage(CardRewardScreen.TEXT[1])
+            .SetOptions(false, false)
+            .AddCallback(m, (enemy, cards) -> cards.get(0).use(AbstractDungeon.player, (AbstractMonster) enemy));
         }
-
     }
 
     @Override
@@ -100,5 +71,34 @@ public class Patchouli extends AnimatorCard implements Spellcaster
             upgradeSecondaryValue(1);
             SetEvokeOrbCount(magicNumber);
         }
+    }
+
+    private DynamicCardBuilder CreateSun()
+    {
+        return new DynamicCardBuilder(cardID + "_Sun")
+        .SetProperties(-2, CardType.SKILL, color, rarity, target)
+        .SetNumbers(0, 0, 5)
+        .SetText(cardStrings.EXTENDED_DESCRIPTION[0], cardStrings.EXTENDED_DESCRIPTION[1], null)
+        .SetOnUse((card, player, enemy) ->
+        {
+            GameActions.Bottom.MakeCardInDiscardPile(new OrbCore_Fire());
+            for (AbstractMonster monster : GameUtilities.GetCurrentEnemies(true))
+            {
+                GameActions.Bottom.ApplyBurning(player, monster, card.magicNumber);
+            }
+        });
+    }
+
+    private DynamicCardBuilder CreateMoon()
+    {
+        return new DynamicCardBuilder(cardID + "_Moon")
+        .SetProperties(-2, CardType.SKILL, color, rarity, target)
+        .SetNumbers(0, 0, 2)
+        .SetText(cardStrings.EXTENDED_DESCRIPTION[2], cardStrings.EXTENDED_DESCRIPTION[3], null)
+        .SetOnUse((card, player, enemy) ->
+        {
+            GameActions.Bottom.MakeCardInDiscardPile(new OrbCore_Frost());
+            GameActions.Bottom.GainMetallicize(card.magicNumber);
+        });
     }
 }

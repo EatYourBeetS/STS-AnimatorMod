d42d20c80b4540e6a773d1d0537a79278ca66544
EatYourBeetSVG <EatYourBeetSVG@gmail.com>
2019-12-28 18:43:37 +0100

    Update Sora.java

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/series/NoGameNoLife/Sora.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/series/NoGameNoLife/Sora.java
@@ -7,28 +7,11 @@ import eatyourbeets.actions.animator.SoraAction;
 import eatyourbeets.cards.base.AnimatorCard;
 import eatyourbeets.cards.base.EYBCardBadge;
 import eatyourbeets.cards.base.Synergies;
-import eatyourbeets.misc.SoraEffects.SoraEffect;
-import eatyourbeets.resources.AnimatorResources;
 import eatyourbeets.utilities.GameActions;
-import patches.AbstractEnums;
 
 public class Sora extends AnimatorCard
 {
     public static final String ID = Register(Sora.class, EYBCardBadge.Special);
-
-    // TODO: Use DynamicCardBuilder
-    public final SoraEffect effect;
-
-    public Sora(SoraEffect effect, String name, String description)
-    {
-        super(staticCardData.get(ID), ID + "Alt", AnimatorResources.GetCardImage(ID + "Alt"),
-                0, CardType.SKILL, AbstractEnums.Cards.THE_ANIMATOR, CardRarity.RARE, CardTarget.ALL);
-
-        this.name = name;
-        this.effect = effect;
-        this.cardText.OverrideDescription(description, "-", true);
-        //this.damageType = this.damageTypeForTurn = DamageInfo.DamageType.THORNS;
-    }
 
     public Sora()
     {
@@ -36,8 +19,6 @@ public class Sora extends AnimatorCard
 
         Initialize(0, 0, 2);
         SetUpgrade(0, 0, 1);
-
-        this.effect = null;
 
         SetMultiDamage(true);
         SetSynergy(Synergies.NoGameNoLife);
@@ -52,11 +33,5 @@ public class Sora extends AnimatorCard
 
         GameActions.Bottom.Add(new WaitAction(0.4f));
         GameActions.Bottom.Add(new SoraAction(name, magicNumber));
-    }
-
-    @Override
-    public boolean canUpgrade()
-    {
-        return effect == null && super.canUpgrade();
     }
 }

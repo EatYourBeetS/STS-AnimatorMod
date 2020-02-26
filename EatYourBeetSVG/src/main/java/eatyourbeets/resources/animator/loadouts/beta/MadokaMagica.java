57e6b1486a203245c8a92bff0efd4a619224e83a
Leozahn <60529411+Leozahn@users.noreply.github.com>
2020-02-26 14:09:41 -0600

    Removed refactor of Kyubey2 and updated MadokaMagica class

--- EatYourBeetSVG/src/main/java/eatyourbeets/resources/animator/loadouts/beta/MadokaMagica.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/resources/animator/loadouts/beta/MadokaMagica.java
@@ -1,6 +1,9 @@
 package eatyourbeets.resources.animator.loadouts.beta;
 
-import eatyourbeets.cards.animator.beta.MadokaMagica.Kyubey;
+import eatyourbeets.cards.animator.basic.Defend;
+import eatyourbeets.cards.animator.basic.Strike;
+import eatyourbeets.cards.animator.beta.MadokaMagica.KyokoSakura;
+import eatyourbeets.cards.animator.colorless.rare.Kyubey;
 import eatyourbeets.cards.base.AnimatorCard_UltraRare;
 import eatyourbeets.cards.base.Synergies;
 import eatyourbeets.resources.animator.misc.AnimatorLoadout;
@@ -9,7 +12,6 @@ import java.util.ArrayList;
 
 public class MadokaMagica extends AnimatorLoadout
 {
-    // Add this loadout in AnimatorPlayerData.AddBetaLoadouts();
     public MadokaMagica()
     {
         super(Synergies.MadokaMagica);
@@ -20,7 +22,21 @@ public class MadokaMagica extends AnimatorLoadout
     @Override
     public ArrayList<String> GetStartingDeck()
     {
-        return new ArrayList<>();
+        if (startingDeck.isEmpty())
+        {
+            startingDeck.add(Strike.DATA.ID);
+            startingDeck.add(Defend.DATA.ID);
+            startingDeck.add(Strike.DATA.ID);
+            startingDeck.add(Strike.DATA.ID);
+            startingDeck.add(Strike.DATA.ID);
+            startingDeck.add(Defend.DATA.ID);
+            startingDeck.add(Defend.DATA.ID);
+            startingDeck.add(Defend.DATA.ID);
+            startingDeck.add(KyokoSakura.DATA.ID);
+            startingDeck.add(KyokoSakura.DATA.ID);
+        }
+
+        return startingDeck;
     }
 
     @Override

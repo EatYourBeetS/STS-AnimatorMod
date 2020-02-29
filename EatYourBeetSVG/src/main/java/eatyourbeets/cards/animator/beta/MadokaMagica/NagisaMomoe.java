3c4028a01717f799825bd74b3bd767e0d4b71c69
EatYourBeetSVG <EatYourBeetSVG@gmail.com>
2020-02-29 12:53:44 +0100

    Charlotte

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/MadokaMagica/NagisaMomoe.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/MadokaMagica/NagisaMomoe.java
@@ -11,6 +11,11 @@ import eatyourbeets.utilities.GameActions;
 public class NagisaMomoe extends AnimatorCard
 {
     public static final EYBCardData DATA = Register(NagisaMomoe.class).SetSkill(1, CardRarity.UNCOMMON, EYBCardTarget.None);
+    static
+    {
+        DATA.AddPreview(new Charlotte(), true);
+        DATA.AddPreview(new Curse_GriefSeed(), false);
+    }
 
     public NagisaMomoe()
     {

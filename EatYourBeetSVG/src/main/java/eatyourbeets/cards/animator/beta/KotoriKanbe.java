1f09a1cda7ac12873fa9ef2faf3beedcbd38b7ae
LeoZahn <leozahn2020@gmail.com>
2020-03-14 11:50:58 -0500

    Updated Rewrite colorless cards

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/KotoriKanbe.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/KotoriKanbe.java
@@ -1,22 +1,26 @@
 package eatyourbeets.cards.animator.beta;
 
+import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.StartupCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.ArtifactPower;
+import eatyourbeets.actions.animator.CreateRandomCurses;
+import eatyourbeets.actions.basic.MoveCard;
 import eatyourbeets.cards.base.AnimatorCard;
 import eatyourbeets.cards.base.EYBCardData;
 import eatyourbeets.cards.base.Synergies;
 import eatyourbeets.utilities.GameActions;
 
-public class KotoriKanbe extends AnimatorCard {
-    public static final EYBCardData DATA = Register(KotoriKanbe.class).SetSkill(2, CardRarity.RARE).SetColor(CardColor.COLORLESS);
+public class KotoriKanbe extends AnimatorCard implements StartupCard {
+    public static final EYBCardData DATA = Register(KotoriKanbe.class).SetSkill(1, CardRarity.RARE).SetColor(CardColor.COLORLESS);
     private static final int HEAL_AMOUNT = 35;
 
     public KotoriKanbe() {
         super(DATA);
 
-        Initialize(0, 0, 5, 5);
-        SetUpgrade(0, 0, 1, 1);
+        Initialize(0, 0, 6);
+        SetUpgrade(0, 0, 2);
+        SetExhaust(true);
 
         SetSynergy(Synergies.Rewrite);
     }
@@ -26,7 +30,15 @@ public class KotoriKanbe extends AnimatorCard {
     {
         GameActions.Bottom.RemovePower(p, m, ArtifactPower.POWER_ID);
         GameActions.Bottom.ApplyWeak(p, m, magicNumber);
-        GameActions.Bottom.ApplyVulnerable(p, m, secondaryValue);
         GameActions.Bottom.Heal(p, m, HEAL_AMOUNT);
+    }
+
+    @Override
+    public boolean atBattleStartPreDraw()
+    {
+        GameActions.Bottom.MoveCard(this, player.discardPile)
+        .ShowEffect(true, true);
+
+        return true;
     }
 }

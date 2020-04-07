14d05082e08850945993b7eb0508300402cd7e96
LeoZahn <leozahn2020@gmail.com>
2020-04-07 11:19:20 -0500

    Close parentheses

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/DateALive/OrigamiTobiichi.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/DateALive/OrigamiTobiichi.java
@@ -83,7 +83,7 @@ public class OrigamiTobiichi extends AnimatorCard
 
         private void InverseOrigamiCheck()
         {
-            if (GameUtilities.GetPowerAmount(SupportDamagePower.POWER_ID) > (supportDamageLimit * amount)
+            if (GameUtilities.GetPowerAmount(SupportDamagePower.POWER_ID) > (supportDamageLimit * amount))
             {
                 GameActions.Bottom.MakeCardInDrawPile(new InverseOrigami()).SetUpgrade(this.upgraded, false);
                 GameActions.Bottom.RemovePower(player, player, this);

c8031a9cb2ccaedd4a14588047c2c431f9e35662
LeoZahn <leozahn2020@gmail.com>
2020-04-24 13:39:04 -0500

    Added Touka

---
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/Rewrite/ToukaNishikujou.java
@@ -0,0 +1,46 @@
+package eatyourbeets.cards.animator.beta.Rewrite;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import eatyourbeets.cards.base.AnimatorCard;
+import eatyourbeets.cards.base.EYBCardData;
+import eatyourbeets.cards.base.EYBCardTarget;
+import eatyourbeets.cards.base.Synergies;
+import eatyourbeets.utilities.GameActions;
+
+public class ToukaNishikujou extends AnimatorCard {
+    public static final EYBCardData DATA = Register(ToukaNishikujou.class).SetSkill(1, CardRarity.COMMON, EYBCardTarget.None);
+
+    public ToukaNishikujou() {
+        super(DATA);
+
+        Initialize(0, 9, 10,1);
+        SetUpgrade(0, 2, -2);
+
+        SetSynergy(Synergies.Rewrite);
+    }
+
+    @Override
+    public void triggerOnManualDiscard()
+    {
+        super.triggerOnManualDiscard();
+
+        GameActions.Bottom.GainAgility(secondaryValue,upgraded);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        GameActions.Bottom.GainBlock(block);
+
+        if (HasSynergy())
+        {
+            int numThrowingKnives = player.currentBlock / magicNumber;
+
+            if (numThrowingKnives > 0)
+            {
+                GameActions.Bottom.CreateThrowingKnives(numThrowingKnives);
+            }
+        }
+    }
+}

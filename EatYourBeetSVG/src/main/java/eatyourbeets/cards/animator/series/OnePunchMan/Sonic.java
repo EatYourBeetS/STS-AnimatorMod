4254b8aa6818a6d78882948584144f8c62777d3a
LeoZahn <leozahn2020@gmail.com>
2020-05-01 11:14:00 -0500

    Reverted Melzalgald and Sonic

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/series/OnePunchMan/Sonic.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/series/OnePunchMan/Sonic.java
@@ -7,7 +7,6 @@ import eatyourbeets.cards.base.AnimatorCard;
 import eatyourbeets.cards.base.EYBCardData;
 import eatyourbeets.cards.base.EYBCardTarget;
 import eatyourbeets.cards.base.Synergies;
-import eatyourbeets.stances.AgilityStance;
 import eatyourbeets.utilities.GameActions;
 
 public class Sonic extends AnimatorCard
@@ -25,8 +24,8 @@ public class Sonic extends AnimatorCard
     {
         super(DATA);
 
-        Initialize(0, 0, 2);
-        SetUpgrade(0, 0, 0);
+        Initialize(0, 0, 2,2);
+        SetUpgrade(0, 0, 0,1);
 
         SetExhaust(true);
         SetSynergy(Synergies.OnePunchMan);
@@ -37,7 +36,7 @@ public class Sonic extends AnimatorCard
     public void use(AbstractPlayer p, AbstractMonster m)
     {
         GameActions.Bottom.GainBlur(magicNumber);
-        GameActions.Bottom.ChangeStance(new AgilityStance());
+        GameActions.Bottom.GainAgility(secondaryValue);
 
         if (HasSynergy())
         {

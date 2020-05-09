fbf93803b5120d4a73965eb269dc687c5c031177
EatYourBeetSVG <EatYourBeetSVG@gmail.com>
2020-05-09 18:26:07 +0200

    Update Saitama.java

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/series/OnePunchMan/Saitama.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/series/OnePunchMan/Saitama.java
@@ -22,35 +22,31 @@ import eatyourbeets.utilities.JavaUtilities;
 public class Saitama extends AnimatorCard
 {
     public static final EYBCardData DATA = Register(Saitama.class).SetSkill(0, CardRarity.RARE, EYBCardTarget.None);
-
     static
     {
-        DATA.AddPreview(new Saitama(1), true);
-        DATA.AddPreview(new Saitama(2), true);
-        DATA.AddPreview(new Saitama(3), true);
-        DATA.AddPreview(new Saitama(4), true);
-        DATA.AddPreview(new Saitama(5), true);
+        DATA.AddPreview(new Saitama(1), false);
+        DATA.AddPreview(new Saitama(2), false);
+        DATA.AddPreview(new Saitama(3), false);
+        DATA.AddPreview(new Saitama(4), false);
+        DATA.AddPreview(new Saitama(5), false);
     }
 
     private int stage;
 
     public Saitama()
     {
-        super(DATA);
-
-        Initialize(0, 0);
-
-        this.misc = 0;
-
-        SetAttackType(EYBAttackType.Normal);
-        SetSynergy(Synergies.OnePunchMan);
+        this(0);
     }
 
     public Saitama(int stage)
     {
-        this();
+        super(DATA);
 
-        this.stage = stage;
+        Initialize(0, 0);
+
+        SetAttackType(EYBAttackType.Normal);
+        SetSynergy(Synergies.OnePunchMan);
+        this.stage = this.misc = stage;
         SetEffect(stage);
     }
 

b43a5f7a69f412a7966b60047b9289483918dc22
LeoZahn <leozahn2020@gmail.com>
2020-05-13 15:45:27 -0500

    Fix for Kurumi + Guy/Yachiyo

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/DateALive/KurumiTokisaki.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/DateALive/KurumiTokisaki.java
@@ -6,11 +6,11 @@ import com.megacrit.cardcrawl.actions.watcher.SkipEnemiesTurnAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.EnergizedPower;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 import com.megacrit.cardcrawl.vfx.BorderFlashEffect;
 import com.megacrit.cardcrawl.vfx.combat.DieDieDieEffect;
 import com.megacrit.cardcrawl.vfx.combat.TimeWarpTurnEndEffect;
 import eatyourbeets.cards.base.*;
-import eatyourbeets.powers.CombatStats;
 import eatyourbeets.utilities.GameActions;
 
 public class KurumiTokisaki extends AnimatorCard
@@ -38,8 +38,13 @@ public class KurumiTokisaki extends AnimatorCard
     {
         super.triggerWhenDrawn();
 
-        GameActions.Bottom.SpendEnergy(costForTurn, false)
-        .AddCallback(() -> GameActions.Bottom.PlayCard(this, null));
+        int energy = EnergyPanel.getCurrentEnergy();
+        if (energy >= costForTurn)
+        {
+            GameActions.Top.PlayCard(this, player.hand, null);
+            GameActions.Top.SpendEnergy(costForTurn, false);
+            GameActions.Top.WaitRealtime(0.3f);
+        }
     }
 
     @Override

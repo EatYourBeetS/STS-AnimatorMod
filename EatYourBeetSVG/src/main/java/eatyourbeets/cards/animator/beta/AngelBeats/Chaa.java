d0da5148c57917a18e626e5092aec7617d00215f
LeoZahn <leozahn2020@gmail.com>
2020-05-16 18:02:35 -0500

    Moved ChaaPower into Chaa

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/AngelBeats/Chaa.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/AngelBeats/Chaa.java
@@ -1,11 +1,15 @@
 package eatyourbeets.cards.animator.beta.AngelBeats;
 
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import eatyourbeets.cards.base.AnimatorCard;
 import eatyourbeets.cards.base.EYBCardData;
 import eatyourbeets.cards.base.Synergies;
-import eatyourbeets.powers.animator.ChaaPower;
+import eatyourbeets.powers.AnimatorPower;
 import eatyourbeets.utilities.GameActions;
 
 public class Chaa extends AnimatorCard
@@ -41,6 +45,47 @@ public class Chaa extends AnimatorCard
                 this.isCostModifiedForTurn = false;
                 fromSynergy = false;
             }
+        }
+    }
+
+    public static class ChaaPower extends AnimatorPower
+    {
+        public static final String POWER_ID = CreateFullID(eatyourbeets.powers.animator.ChaaPower.class);
+
+        public ChaaPower(AbstractCreature owner, int amount)
+        {
+            super(owner, Chaa.DATA);
+            this.amount = amount;
+            updateDescription();
+        }
+
+        @Override
+        public void atStartOfTurnPostDraw()
+        {
+            GameActions.Bottom.ExhaustFromHand(name, amount, false)
+                    .SetOptions(false, false, false)
+                    .AddCallback(cards ->
+                    {
+                        for (AbstractCard card : cards)
+                        {
+                            AbstractCard c;
+                            if (card.rarity == AbstractCard.CardRarity.RARE || card.rarity == AbstractCard.CardRarity.UNCOMMON || card.rarity == AbstractCard.CardRarity.SPECIAL) {
+                                c =  AbstractDungeon.getCard(AbstractCard.CardRarity.RARE).makeCopy();
+                            } else if (card.rarity == AbstractCard.CardRarity.COMMON) {
+                                c =  AbstractDungeon.getCard(AbstractCard.CardRarity.UNCOMMON).makeCopy();
+                            } else {
+                                c =  AbstractDungeon.getCard(AbstractCard.CardRarity.COMMON).makeCopy();
+                            }
+                            addToBot(new MakeTempCardInHandAction(c));
+                        }
+                    });
+        }
+
+        @Override
+        public void updateDescription()
+        {
+            this.description = FormatDescription(0, amount);
+            this.enabled = (amount > 0);
         }
     }
 }

758b4fff4c2d2765675978d13f496515bd8b257c
EatYourBeetSVG <EatYourBeetSVG@gmail.com>
2020-05-30 14:47:01 +0200

    Improved SelectCreature action

--- EatYourBeetSVG/src/main/java/eatyourbeets/actions/special/SelectCreature.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/actions/special/SelectCreature.java
@@ -6,42 +6,124 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Bezier;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.GameCursor;
 import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.helpers.input.InputHelper;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import eatyourbeets.actions.EYBActionWithCallback;
+import eatyourbeets.cards.base.EYBCard;
 import eatyourbeets.resources.GR;
 import eatyourbeets.utilities.GameUtilities;
+import eatyourbeets.utilities.JavaUtilities;
 
 public class SelectCreature extends EYBActionWithCallback<AbstractCreature>
 {
+    public enum Targeting
+    {
+        Player,
+        Enemy,
+        Any,
+        Random,
+        AoE,
+        None
+    }
+
+    protected AbstractCreature previous;
     protected AbstractCreature target;
-    protected Vector2 controlPoint;
-    protected boolean includePlayer;
+    protected Targeting targeting;
+    protected boolean skipConfirmation;
 
+    private final Vector2[] points = new Vector2[20];
+    private final Vector2 controlPoint = new Vector2();
+    private final Vector2 origin = new Vector2();
     private float arrowScaleTimer;
-    private Vector2[] points = new Vector2[20];
 
-    public SelectCreature(boolean includePlayer)
+    public SelectCreature(Targeting targeting)
     {
         super(ActionType.SPECIAL);
+
+        this.card = null;
+        this.targeting = targeting;
 
         Initialize(amount);
     }
 
-    public SelectCreature SetControlPoint(Vector2 point)
+    public SelectCreature(AbstractCard card)
     {
-        controlPoint = point;
+        super(ActionType.SPECIAL);
 
-        return this;
+        this.card = card;
+
+        EYBCard c = JavaUtilities.SafeCast(card, EYBCard.class);
+        if (c != null && c.attackTarget != null)
+        {
+            switch (c.attackTarget)
+            {
+                case Self:
+                    targeting = Targeting.Player;
+                    break;
+                case None:
+                    targeting = Targeting.None;
+                    break;
+                case Normal:
+                    targeting = Targeting.Enemy;
+                    break;
+                case ALL:
+                    targeting = Targeting.AoE;
+                    break;
+                case Random:
+                    targeting = Targeting.Random;
+                    break;
+            }
+        }
+        else
+        {
+            switch (card.target)
+            {
+                case ENEMY:
+                case SELF_AND_ENEMY:
+                    targeting = Targeting.Enemy;
+                    break;
+                case ALL:
+                case ALL_ENEMY:
+                    targeting = Targeting.AoE;
+                    break;
+                case SELF:
+                    targeting = Targeting.Player;
+                    break;
+                case NONE:
+                    targeting = Targeting.None;
+                    break;
+            }
+        }
+
+        Initialize(amount);
     }
 
     @Override
     protected void FirstUpdate()
     {
+        if (skipConfirmation)
+        {
+            switch (targeting)
+            {
+                case Player:
+                    Complete(AbstractDungeon.player);
+                    return;
+                case Random:
+                    Complete(GameUtilities.GetRandomEnemy(true));
+                    return;
+                case AoE:
+                case None:
+                    Complete(null);
+                    return;
+            }
+        }
+
         for (int i = 0; i < this.points.length; ++i)
         {
             this.points[i] = new Vector2();
@@ -61,13 +143,60 @@ public class SelectCreature extends EYBActionWithCallback<AbstractCreature>
             return;
         }
 
-        target = null;
+        switch (targeting)
+        {
+            case Player:
+                UpdateTarget(true, false);
+                break;
+            case Enemy:
+                UpdateTarget(false, true);
+                break;
+            case Any:
+                UpdateTarget(true, true);
+                break;
+        }
 
-        if (includePlayer && player.hb.hovered && !player.isDying)
+        if (InputHelper.justClickedLeft)
+        {
+            InputHelper.justClickedLeft = false;
+            switch (targeting)
+            {
+                case Random:
+                    Complete(GameUtilities.GetRandomEnemy(true));
+                    return;
+
+                case AoE:
+                case None:
+                    Complete(null);
+                    return;
+
+                case Player:
+                case Enemy:
+                case Any:
+                    if (target != null)
+                    {
+                        Complete(target);
+                        return;
+                    }
+            }
+        }
+
+        GR.UI.AddPostRender(this::Render);
+    }
+
+    protected void UpdateTarget(boolean targetPlayer, boolean targetEnemy)
+    {
+        if (target != null)
+        {
+            previous = target;
+            target = null;
+        }
+
+        if (targetPlayer && (player.hb.hovered && !player.isDying))
         {
             target = player;
         }
-        else
+        else if (targetEnemy)
         {
             for (AbstractMonster m : GameUtilities.GetEnemies(true))
             {
@@ -79,29 +208,66 @@ public class SelectCreature extends EYBActionWithCallback<AbstractCreature>
             }
         }
 
-        if (InputHelper.justClickedLeft)
+        if ((card != null) && (target != null) && (target != previous))
         {
-            InputHelper.justClickedLeft = false;
-            if (target != null)
+            if (target instanceof AbstractMonster)
             {
-                Complete(target);
-                return;
+                card.calculateCardDamage((AbstractMonster)target);
+            }
+            else
+            {
+                card.applyPowers();
             }
         }
-
-        GR.UI.AddPostRender(this::Render);
     }
 
     public void Render(SpriteBatch sb)
     {
+        switch (targeting)
+        {
+            case Player:
+            case Enemy:
+            case Any:
+                RenderArrow(sb);
+                if (target != null)
+                {
+                    target.renderReticle(sb);
+                }
+                break;
+
+            case Random:
+            case AoE:
+                for (AbstractCreature c : GameUtilities.GetAllCharacters(true))
+                {
+                    c.renderReticle(sb);
+                }
+                break;
+
+            case None:
+                break;
+        }
+    }
+
+    protected void RenderArrow(SpriteBatch sb)
+    {
         float x = (float) InputHelper.mX;
         float y = (float) InputHelper.mY;
 
-        Vector2 origin = controlPoint;
-
-        if (origin == null)
+        if (card != null)
         {
-            origin = new Vector2(player.animX - (x - player.animX) / 4f, player.animY + (y - player.animY - 40f * Settings.scale) / 2f);
+            origin.x = card.current_x;
+            origin.y = card.current_y;
+
+            controlPoint.x = card.current_x - ((x - card.current_x) / 4f);
+            controlPoint.y = card.current_y + ((y - card.current_y - 40f * Settings.scale) / 2f);
+        }
+        else
+        {
+            origin.x = player.dialogX;
+            origin.y = player.dialogY - 40f * Settings.scale;
+
+            controlPoint.x = player.animX - (x - player.animX) / 4f;
+            controlPoint.y = player.animY + (y - player.animY - 40f * Settings.scale) / 2f;
         }
 
         float arrowScale;
@@ -123,18 +289,14 @@ public class SelectCreature extends EYBActionWithCallback<AbstractCreature>
             sb.setColor(new Color(1f, 0.2F, 0.3F, 1f));
         }
 
-        Vector2 tmp = new Vector2(origin.x - x, origin.y - y);
+        Vector2 tmp = new Vector2(controlPoint.x - x, controlPoint.y - y);
         tmp.nor();
-        DrawCurve(sb, new Vector2(player.dialogX, player.dialogY - 40f * Settings.scale), new Vector2(x, y), origin);
-        sb.draw(ImageMaster.TARGET_UI_ARROW, x - 128f, y - 128f, 128f, 128f, 256f, 256f, arrowScale, arrowScale, tmp.angle() + 90f, 0, 0, 256, 256, false, false);
-
-        if (target != null)
-        {
-            target.renderReticle(sb);
-        }
+        DrawCurve(sb, origin, new Vector2(x, y), controlPoint);
+        sb.draw(ImageMaster.TARGET_UI_ARROW, x - 128f, y - 128f, 128f, 128f, 256f, 256f, arrowScale, arrowScale,
+        tmp.angle() + 90f, 0, 0, 256, 256, false, false);
     }
 
-    private void DrawCurve(SpriteBatch sb, Vector2 start, Vector2 end, Vector2 control)
+    protected void DrawCurve(SpriteBatch sb, Vector2 start, Vector2 end, Vector2 control)
     {
         float radius = 7f * Settings.scale;
 

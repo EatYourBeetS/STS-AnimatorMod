3b9952b23860f118022cca343d5a46499e67e00c
LeoZahn <leozahn2020@gmail.com>
2020-06-08 18:07:43 -0500

    Yoshino (Rewrite) redesign

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/Rewrite/YoshinoHaruhiko.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/Rewrite/YoshinoHaruhiko.java
@@ -7,7 +7,6 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import eatyourbeets.cards.base.*;
 import eatyourbeets.cards.base.attributes.AbstractAttribute;
-import eatyourbeets.powers.common.SelfDamagePower;
 import eatyourbeets.stances.ForceStance;
 import eatyourbeets.utilities.GameActions;
 
@@ -19,8 +18,9 @@ public class YoshinoHaruhiko extends AnimatorCard
     {
         super(DATA);
 
-        Initialize(3, 0, 6, 2);
-        SetUpgrade(1, 0, -2);
+        Initialize(3, 0, 2);
+        SetUpgrade(0, 0, 1);
+        SetExhaust(true);
 
         SetSynergy(Synergies.Rewrite);
         SetMartialArtist();
@@ -29,18 +29,20 @@ public class YoshinoHaruhiko extends AnimatorCard
     @Override
     public AbstractAttribute GetDamageInfo()
     {
-        return super.GetDamageInfo().AddMultiplier(secondaryValue);
+        return super.GetDamageInfo().AddMultiplier(magicNumber);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m)
     {
-        for (int i = 0; i < secondaryValue; i++)
+        for (int i = 0; i < magicNumber; i++)
         {
             GameActions.Bottom.DealDamageToRandomEnemy(damage, DamageInfo.DamageType.NORMAL, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
         }
 
-        if (player.stance.ID.equals(ForceStance.STANCE_ID))
+        GameActions.Bottom.ChangeStance(ForceStance.STANCE_ID);
+
+        if (IsStarter())
         {
             for (AbstractCard card : player.hand.group)
             {
@@ -50,11 +52,6 @@ public class YoshinoHaruhiko extends AnimatorCard
                     card.flash();
                 }
             }
-        }
-        else
-        {
-            GameActions.Bottom.ChangeStance(ForceStance.STANCE_ID);
-            GameActions.Bottom.StackPower(new SelfDamagePower(p, magicNumber));
         }
     }
 }

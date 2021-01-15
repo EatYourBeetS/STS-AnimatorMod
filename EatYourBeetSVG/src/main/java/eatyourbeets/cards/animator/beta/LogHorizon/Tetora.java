7023752d026d888f368a3fc3d1e760b503076222
LeoZahn <leozahn2020@gmail.com>
2021-01-14 18:44:36 -0600

    Tetora changes, Tohya can draw Minori

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/LogHorizon/Tetora.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/LogHorizon/Tetora.java
@@ -19,8 +19,8 @@ public class Tetora extends AnimatorCard
     {
         super(DATA);
 
-        Initialize(0, 0, 4, 1);
-        SetUpgrade(0, 0, 1, 0);
+        Initialize(0, 0, 3, 1);
+        SetUpgrade(0, 0, 2, 0);
 
         SetSpellcaster();
         SetSynergy(Synergies.LogHorizon);
--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/LogHorizon/Tohya.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/LogHorizon/Tohya.java
@@ -1,20 +1,21 @@
 package eatyourbeets.cards.animator.beta.LogHorizon;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import eatyourbeets.cards.base.AnimatorCard;
 import eatyourbeets.cards.base.EYBAttackType;
 import eatyourbeets.cards.base.EYBCardData;
 import eatyourbeets.cards.base.Synergies;
+import eatyourbeets.ui.cards.DrawPileCardPreview;
 import eatyourbeets.utilities.GameActions;
-import eatyourbeets.utilities.GameEffects;
-import eatyourbeets.utilities.GameUtilities;
 
 public class Tohya extends AnimatorCard {
     public static final EYBCardData DATA = Register(Tohya.class).SetAttack(1, CardRarity.COMMON, EYBAttackType.Normal);
+
+    private final DrawPileCardPreview drawPileCardPreview = new DrawPileCardPreview(Tohya::FindMinori);
 
     static
     {
@@ -37,10 +38,7 @@ public class Tohya extends AnimatorCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         GameActions.Bottom.DealDamage(this, m, AbstractGameAction.AttackEffect.SLASH_VERTICAL);
 
-        GameActions.Bottom.Callback(() ->
-        {
-            DrawMinori(player.drawPile);
-        });
+        GameActions.Bottom.Callback(this::DrawMinori);
 
         if (IsStarter())
         {
@@ -48,23 +46,58 @@ public class Tohya extends AnimatorCard {
         }
     }
 
-    private boolean DrawMinori(CardGroup group)
+    @Override
+    public void calculateCardDamage(AbstractMonster mo)
     {
-        for (AbstractCard c : group.group)
+        super.calculateCardDamage(mo);
+
+        drawPileCardPreview.SetCurrentTarget(mo);
+    }
+
+    @Override
+    public void update()
+    {
+        super.update();
+
+        drawPileCardPreview.Update();
+    }
+
+    @Override
+    public void Render(SpriteBatch sb, boolean hovered, boolean selected, boolean library)
+    {
+        super.Render(sb, hovered, selected, library);
+
+        if (!library)
+        {
+            drawPileCardPreview.Render(sb);
+        }
+    }
+
+    private static AbstractCard FindMinori(AbstractMonster target)
+    {
+        AbstractCard minori = null;
+        int maxDamage = Integer.MIN_VALUE;
+        for (AbstractCard c : player.drawPile.group)
         {
             if (Minori.DATA.ID.equals(c.cardID))
             {
-                if (group.type != CardGroup.CardGroupType.HAND)
-                {
-                    GameEffects.List.ShowCardBriefly(makeStatEquivalentCopy());
-                    GameActions.Top.MoveCard(c, group, player.hand)
-                            .ShowEffect(true, true)
-                            .AddCallback(GameUtilities::Retain);
-                }
-
-                return true;
+                minori = c;
             }
         }
+
+        return minori;
+    }
+
+    private boolean DrawMinori()
+    {
+       AbstractCard minori = FindMinori(null);
+
+       if (minori != null)
+       {
+           GameActions.Top.MoveCard(minori, player.hand)
+                   .ShowEffect(true, true);
+           return true;
+       }
 
         return false;
     }

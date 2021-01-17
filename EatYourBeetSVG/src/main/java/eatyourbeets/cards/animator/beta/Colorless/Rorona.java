3d4dc358a446cd2e1e59469455ab938938bcc312
LeoZahn <leozahn2020@gmail.com>
2021-01-16 20:53:10 -0600

    Added Shuu Ouma and Miku

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/Colorless/Rorona.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/Colorless/Rorona.java
@@ -1,4 +1,4 @@
-package eatyourbeets.cards.animator.beta;
+package eatyourbeets.cards.animator.beta.Colorless;
 
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;

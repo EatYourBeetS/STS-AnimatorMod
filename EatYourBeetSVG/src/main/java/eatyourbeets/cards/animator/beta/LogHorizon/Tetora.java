5ed395986a6cd164c21b59899a9bad7e3154cfcb
LeoZahn <leozahn2020@gmail.com>
2021-01-18 18:35:30 -0600

    Tetora brought down to 2

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/LogHorizon/Tetora.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/LogHorizon/Tetora.java
@@ -19,7 +19,7 @@ public class Tetora extends AnimatorCard
     {
         super(DATA);
 
-        Initialize(0, 0, 3, 3);
+        Initialize(0, 0, 2, 3);
         SetUpgrade(0, 0, 0, 2);
 
         SetSpellcaster();

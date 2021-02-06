ee334f2ca936764be43ecf82dd262eca593ad2b6
LeoZahn <leozahn2020@gmail.com>
2021-02-06 12:42:46 -0600

    Nao redesign

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/Colorless/NaoTomori.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/Colorless/NaoTomori.java
@@ -4,6 +4,7 @@ import basemod.BaseMod;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.cards.curses.Normality;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -17,7 +18,7 @@ import eatyourbeets.utilities.GameUtilities;
 
 public class NaoTomori extends AnimatorCard
 {
-    public static final EYBCardData DATA = Register(NaoTomori.class).SetSkill(2, CardRarity.RARE, EYBCardTarget.None).SetColor(CardColor.COLORLESS);
+    public static final EYBCardData DATA = Register(NaoTomori.class).SetSkill(1, CardRarity.RARE, EYBCardTarget.None).SetColor(CardColor.COLORLESS);
 
     protected static final Color weakenedGlowColor = Color.RED;
 
@@ -27,10 +28,14 @@ public class NaoTomori extends AnimatorCard
 
         Initialize(0, 0, 3);
         SetUpgrade(0, 0, 0);
-        SetCostUpgrade(-1);
-        SetRetain(true);
 
         SetSynergy(Synergies.Charlotte);
+    }
+
+    @Override
+    protected void OnUpgrade()
+    {
+        SetRetain(true);
     }
 
     @Override
@@ -67,13 +72,17 @@ public class NaoTomori extends AnimatorCard
     @Override
     public void use(AbstractPlayer p, AbstractMonster m)
     {
-        GameActions.Bottom.ExhaustFromHand(name, 1, false)
+        GameActions.Bottom.SelectFromHand(name, 1, false)
                 .SetFilter(c -> c.type.equals(CardType.POWER) || c.type.equals(CardType.CURSE))
                 .SetOptions(false, false, false)
                 .AddCallback(cards ->
                 {
                     if (cards.size() > 0)
                     {
+                        AbstractCard card = (cards.get(0));
+
+                        GameActions.Bottom.ReplaceCard(card.uuid, new Normality())
+                                .SetUpgrade(card.upgraded);
                         GameActions.Bottom.StackPower(new PhasingPower(p, secondaryValue));
                     }
                 });

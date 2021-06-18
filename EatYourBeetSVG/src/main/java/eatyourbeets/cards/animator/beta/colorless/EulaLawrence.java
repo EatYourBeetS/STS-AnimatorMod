6c0c3a04dcfe82263b57a66ae1c854d29c89166d
SevenDayCandle <persisting_delusions@yahoo.com>
2021-06-18 14:44:02 -0700

    - Initial versions of Barbara Pegg, Eula Lawrence, and Jean Gunnhildr

---
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/beta/colorless/EulaLawrence.java
@@ -0,0 +1,60 @@
+package eatyourbeets.cards.animator.beta.colorless;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import eatyourbeets.cards.animator.beta.special.SongOfBrokenPines;
+import eatyourbeets.cards.animator.special.ShichikaKyotouryuu;
+import eatyourbeets.cards.base.AnimatorCard;
+import eatyourbeets.cards.base.EYBCardData;
+import eatyourbeets.cards.base.Synergies;
+import eatyourbeets.powers.CombatStats;
+import eatyourbeets.utilities.GameActions;
+import eatyourbeets.utilities.RandomizedList;
+
+public class EulaLawrence extends AnimatorCard {
+    public static final EYBCardData DATA = Register(EulaLawrence.class).SetAttack(3, CardRarity.RARE);
+    static
+    {
+        DATA.AddPreview(new SongOfBrokenPines(), true);
+    }
+
+    public EulaLawrence() {
+        super(DATA);
+
+        Initialize(18, 0, 0);
+        SetUpgrade(2, 0, 0);
+        SetScaling(0, 0, 2);
+
+        SetSynergy(Synergies.GenshinImpact);
+    }
+
+    @Override
+    public boolean HasDirectSynergy(AbstractCard other)
+    {
+        return (other.rarity.equals(CardRarity.BASIC)) || super.HasDirectSynergy(other);
+    }
+
+    @Override
+    public void OnUse(AbstractPlayer p, AbstractMonster m, boolean isSynergizing) {
+
+        GameActions.Bottom.DealDamage(this, m, AbstractGameAction.AttackEffect.SLASH_HEAVY);
+
+        // Set the cost of a random Basic card in your draw pile to 0 until played
+        RandomizedList<AbstractCard> randomizedList = new RandomizedList<>();
+        randomizedList.AddAll(player.drawPile.group);
+        AbstractCard card = randomizedList.Retrieve(rng);
+
+        if (card != null && card.rarity.equals(CardRarity.BASIC))
+        {
+            card.freeToPlayOnce = true;
+        }
+
+        // Create Song of Broken Pines if you have at least 15 cards in your draw pile
+        if (HasSynergy() && CombatStats.TryActivateLimited(cardID)) {
+            GameActions.Bottom.MakeCardInDrawPile(new SongOfBrokenPines());
+        }
+
+    }
+}

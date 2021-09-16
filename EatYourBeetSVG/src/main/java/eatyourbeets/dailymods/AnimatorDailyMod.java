d440381bc2dd4bf3ae4edecfc7acbee4cc855919
Leozahn <60529411+Leozahn@users.noreply.github.com>
2021-09-15 22:00:53 -0500

    Trying to get strings to show up in-game

--- EatYourBeetSVG/src/main/java/eatyourbeets/dailymods/AnimatorDailyMod.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/dailymods/AnimatorDailyMod.java
@@ -1,18 +1,17 @@
 package eatyourbeets.dailymods;
 
-import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.daily.mods.AbstractDailyMod;
 import com.megacrit.cardcrawl.localization.RunModStrings;
+import eatyourbeets.resources.GR;
 
 public class AnimatorDailyMod extends AbstractDailyMod {
 
     private static final String TESTIMAGE = "draft.png";
 
-    public AnimatorDailyMod(String modID, boolean positive) {
-        super(modID, "", "", TESTIMAGE, positive);
+    public AnimatorDailyMod(String modName, boolean positive) {
+        super(modName, "", "", TESTIMAGE, positive);
 
-        String ID = modID;
-        RunModStrings modStrings = CardCrawlGame.languagePack.getRunModString(ID);
+        RunModStrings modStrings = GR.GetRunModStrings(modName);
         this.name = modStrings.NAME;
         this.description = modStrings.DESCRIPTION;
     }
--- EatYourBeetSVG/src/main/java/eatyourbeets/resources/GR.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/resources/GR.java
@@ -134,6 +134,11 @@ public class GR
         return GetLanguagePack().getUIString(stringID);
     }
 
+    public static RunModStrings GetRunModStrings(String stringID)
+    {
+        return GetLanguagePack().getRunModString(stringID);
+    }
+
     public static OrbStrings GetOrbStrings(String orbID)
     {
         return GetLanguagePack().getOrbString(orbID);

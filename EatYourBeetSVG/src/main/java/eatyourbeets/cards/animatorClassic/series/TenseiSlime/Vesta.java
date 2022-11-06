eb143cddde86e29a0e363031cf171bf31d4c03eb
SevenDayCandle <persisting_delusions@yahoo.com>
2022-11-06 09:39:46 -0800

    - Vesta

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animatorClassic/series/TenseiSlime/Vesta.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animatorClassic/series/TenseiSlime/Vesta.java
@@ -2,11 +2,21 @@ package eatyourbeets.cards.animatorClassic.series.TenseiSlime;
 
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import eatyourbeets.cards.animatorClassic.special.Vesta_Elixir;
 import eatyourbeets.cards.base.AnimatorClassicCard;
 import eatyourbeets.cards.base.CardUseInfo;
 import eatyourbeets.cards.base.EYBCardData;
 import eatyourbeets.cards.base.EYBCardTarget;
+import eatyourbeets.cards.effects.VestaElixirEffects.AnimatorClassicVestaElixirEffects;
+import eatyourbeets.cards.effects.VestaElixirEffects.VestaElixirEffect;
+import eatyourbeets.cards.effects.VestaElixirEffects.VestaElixirEffect_CompleteFaster;
+import eatyourbeets.cards.effects.VestaElixirEffects.VestaElixirEffects;
 import eatyourbeets.interfaces.subscribers.OnStartOfTurnPostDrawSubscriber;
+import eatyourbeets.powers.CombatStats;
+import eatyourbeets.utilities.GameActions;
+import eatyourbeets.utilities.GameEffects;
+
+import java.util.ArrayList;
 
 public class Vesta extends AnimatorClassicCard implements OnStartOfTurnPostDrawSubscriber
 {
@@ -15,11 +25,11 @@ public class Vesta extends AnimatorClassicCard implements OnStartOfTurnPostDrawS
             .SetSeriesFromClassPackage();
     static
     {
-        //DATA.AddPreview(new Vesta_Elixir(), false);
+        DATA.AddPreview(new Vesta_Elixir(), false);
     }
 
     private int timer;
-    //private Vesta_Elixir elixir;
+    private Vesta_Elixir elixir;
 
     public Vesta()
     {
@@ -32,53 +42,47 @@ public class Vesta extends AnimatorClassicCard implements OnStartOfTurnPostDrawS
     }
 
     @Override
-    public void OnStartOfTurnPostDraw()
+    public void OnUse(AbstractPlayer p, AbstractMonster m, CardUseInfo info)
     {
+        AnimatorClassicVestaElixirEffects.BeginCreateElixir((Vesta) this.makeStatEquivalentCopy(), this.upgraded);
+    }
 
+    public void ResearchElixir(Vesta_Elixir elixir)
+    {
+        this.elixir = elixir;
+        this.timer = magicNumber;
+
+        ArrayList<VestaElixirEffect> effects = new ArrayList<>();
+        for (VestaElixirEffect effect : elixir.effects)
+        {
+            if (effect instanceof VestaElixirEffect_CompleteFaster)
+            {
+                this.timer -= 1;
+            }
+            else
+            {
+                effects.add(effect);
+            }
+        }
+
+        this.elixir.ApplyEffects(effects);
+
+        CombatStats.onStartOfTurnPostDraw.Subscribe(this);
     }
 
     @Override
-    public void OnUse(AbstractPlayer p, AbstractMonster m, CardUseInfo info)
+    public void OnStartOfTurnPostDraw()
     {
-        //VestaElixirEffects.BeginCreateElixir((Vesta) this.makeStatEquivalentCopy());
+        if (timer > 0)
+        {
+            timer -= 1;
+        }
+        else
+        {
+            GameEffects.Queue.ShowCardBriefly(this);
+            GameActions.Bottom.MakeCardInHand(elixir);
+
+            CombatStats.onStartOfTurnPostDraw.Unsubscribe(this);
+        }
     }
-//
-//    public void ResearchElixir(Vesta_Elixir elixir)
-//    {
-//        this.elixir = elixir;
-//        this.timer = magicNumber;
-//
-//        ArrayList<VestaElixirEffect> effects = new ArrayList<>();
-//        for (VestaElixirEffect effect : elixir.effects)
-//        {
-//            if (effect instanceof VestaElixirEffect_CompleteFaster)
-//            {
-//                this.timer -= 1;
-//            }
-//            else
-//            {
-//                effects.add(effect);
-//            }
-//        }
-//
-//        this.elixir.ApplyEffects(effects);
-//
-//        CombatStats.onStartOfTurnPostDraw.Subscribe(this);
-//    }
-//
-//    @Override
-//    public void OnStartOfTurnPostDraw()
-//    {
-//        if (timer > 0)
-//        {
-//            timer -= 1;
-//        }
-//        else
-//        {
-//            GameEffects.Queue.ShowCardBriefly(this);
-//            GameActions.Bottom.MakeCardInHand(elixir);
-//
-//            CombatStats.onStartOfTurnPostDraw.Unsubscribe(this);
-//        }
-//    }
 }
